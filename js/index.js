eb06d849fd07083135040189f7a33024c511c68c
Ikeh Akinyemi <mrikehchukwuka@gmail.com>
2021-02-14 01:25:06 +0100

    start with the Nav UI layout

--- js/index.js
+++ js/index.js
@@ -1,27 +1,41 @@
-// control mobile view navlink slide-in and slide-out animations
 const menu = document.querySelector('[alt="menu"]');
 const cancelBtn = document.querySelector('.cancel-icon');
 const sideNav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-link");
 const body = document.querySelector("body");
 
+// control mobile view navlink slide-in and slide-out animations
 menu.addEventListener("click", () => {
   sideNav.classList.toggle("open-nav-links");
 });
 
-sideNav.addEventListener("click", () => {
+cancelBtn.addEventListener("click", () => {
   setTimeout(() => {
     sideNav.classList.toggle("open-nav-links");
-  }, 500);
+  }, 10);
+});
+
+navLinks.forEach((li) => {
+  li.addEventListener("mouseover", (e) => {
+    const liFlag = Boolean(e.target.closest(".nav-link").firstElementChild?.className);
+    if (liFlag &&  window.innerWidth < 912) {
+      e.target.closest(".nav-link").firstElementChild.style.display = "block";
+    }
+  });
+
+  li.addEventListener("mouseout",(e) => {
+    const liFlag = Boolean(e.target.closest(".nav-link").firstElementChild?.className);
+    if (liFlag && window.innerWidth < 912 ) {
+      e.target.closest(".nav-link").firstElementChild.style.display = "none";
+    }
+  });
 });
 
 // control desktop modal pop-in and pop-out
-const navLinks = document.querySelectorAll(".nav-link");
-
 navLinks.forEach(li => {
   li.addEventListener("mouseover",(e) => {
     if ( window.innerWidth > 912 ) {
       if (e.target.closest(".nav-link")) {
-        console.log(e.target.closest(".nav-link").firstElementChild)
         e.target.closest(".nav-link").firstElementChild.style.display = "block";
       }
     }
